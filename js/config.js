7c9cbd9dbbbbc8b1025d1e5a87a4d6fc67cd30a6
JuanPabloSch <juanpablosch@gmail.com>
2026-06-17 23:21:42 -0300

    new+

--- js/config.js
+++ js/config.js
@@ -20,9 +20,9 @@ window.retratoDerecho = null;
 window.baseDeDatosEquipos = {
     "LAN": { nombre: "Lanus", carpeta: "lanus", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0x2980b9, arquero: "Lechuga", pateadores: ["Huguito", "Chango", "Caño", "Chupa"] },
     "BOK": { nombre: "Boca", carpeta: "boca", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" },colorRopa: 0xf1c40f, arquero: "El_Mono", pateadores: ["Cagna", "Fabri", "Manteca", "Alphonse"] },
-    "RIV": { nombre: "River", carpeta: "river", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0xff0000, arquero: "Burgos", pateadores: ["Francescoli", "Ortega", "Gallardo", "Salas"] },
-    "RAC": { nombre: "Racing", carpeta: "racing", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0x00a8e8, arquero: "Nacho", pateadores: ["Capria", "Delgado", "Ubeda", "Marini"] },
-    "IND": { nombre: "Indepte", carpeta: "indepte", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0xff0000, arquero: "Mondragon", pateadores: ["Burruchaga", "Calderon", "Serrizuela", "Mazzoni"] },
+    "RIV": { nombre: "River", carpeta: "river", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0xff0000, arquero: "Burgos", pateadores: ["El_Enzo", "El_Burrito", "Crespo", "Almeyda"] },
+    "RAC": { nombre: "Racing", carpeta: "racing", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0x00a8e8, arquero: "Nacho", pateadores: ["Mago", "Chelo", "Ubeda", "Piojo"] },
+    "IND": { nombre: "Indepte", carpeta: "independiente", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0xff0000, arquero: "Faryd", pateadores: ["Burru", "Panchito", "Morron", "Cascini"] },
     "NEW": { nombre: "Newells", carpeta: "newells", sprites: { idle: "negrouu_idle", vuelo: "negrouu_vuelo", shoot: "indio_shoot" }, colorRopa: 0xff0000, arquero: "Cejas", pateadores: ["Manso", "Marioni", "Oliendo", "Saldaña"] }
 };
 
